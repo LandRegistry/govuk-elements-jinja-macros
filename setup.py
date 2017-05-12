38959e74aa4a13e356d572374326b66a599a0ae7
Andy Mantell <andymantell@hotmail.com>
2017-05-12 12:22:51 +0100

    Tweak setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,5 @@ from govuk_elements_jinja_macros.version import Version
 setuptools.setup(name='govuk-elements-jinja-macros',
                  version=Version('1.0.0').number,
                  description='GOV.UK elements Jinja macros',
-                 packages=['govuk_elements_jinja_macros'],
-                 install_requires=[],
-                 zip_safe=False
+                 packages=['govuk_elements_jinja_macros']
                  )
