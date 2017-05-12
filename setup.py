ea5e45cb9b71632e96d49af647cb7aaf1b5e86c6
Andy Mantell <andymantell@hotmail.com>
2017-05-12 12:05:58 +0100

    Rename package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
-from govuk_jinja_macros.version import Version
+from govuk_elements_jinja_macros.version import Version
 
 
-setuptools.setup(name='govuk-jinja-macros',
+setuptools.setup(name='govuk-elements-jinja-macros',
                  version=Version('1.0.0').number,
-                 description='GOV.UK Jinja macros',
-                 packages=['govuk_jinja_macros'],
+                 description='GOV.UK elements Jinja macros',
+                 packages=['govuk_elements_jinja_macros'],
                  install_requires=[],
                  zip_safe=False
                  )
